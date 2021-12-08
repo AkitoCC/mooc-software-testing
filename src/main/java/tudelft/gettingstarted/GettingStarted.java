5dac5eb04509ad3df97004b381acf4b8fcb2d7d2
AkitoCC <nuno_barbosa21@hotmail.com>
2021-12-08 15:09:09 +0000

    Complete test class for addFive

--- src/main/java/tudelft/gettingstarted/GettingStarted.java
+++ src/main/java/tudelft/gettingstarted/GettingStarted.java
@@ -1,5 +1,7 @@
 package tudelft.gettingstarted;
 
+import javax.xml.transform.stream.StreamSource;
+
 public class GettingStarted {
     public int addFive (int number) {
         return number + 5;
